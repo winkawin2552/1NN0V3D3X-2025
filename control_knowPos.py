a5a87ee9a4a57414884ec96cafa7ed7f34360fab
winkawin2552 <167737245+winkawin2552@users.noreply.github.com>
2025-08-07 20:29:20 +0700

    update control_fix angle

--- control_knowPos.py
+++ control_knowPos.py
@@ -36,7 +36,7 @@ class Servo:
         self.current_angle = target_angle
  
 power_gripper = 95
-off_gripper = 42
+off_gripper = 47
  
 def setup(servo_pins = servo_pins):
     for pin in servo_pins:
@@ -56,14 +56,12 @@ def check():
     pos(pos = [242,99,150,0,0], step_delay=0.01)
  
 def grab_normal(base):
-    servos[5].write(20)
-    servos[6].write(156)
     pos(pos = [base,0,156,169,power_gripper])
 
 def grab_ad(base):
-    servos[5].write(20)
-    servos[6].write(141)
-    pos(pos = [base,0,141,159,power_gripper])
+    servos[6].write(146)
+    servos[7].write(154)
+    pos(pos = [base,10,146,154,power_gripper])
 
 def drop_normal(base):
     pos(pos = [base,13,156,180,off_gripper])
@@ -79,36 +77,44 @@ def st1(base = 125):
     grab_ad(base)if base == use_pos[2] else grab_normal(base)
 
 def st2(base = 125):
-    servos[6].write(130)
+    servos[6].write(120) # 130
     drop_ad(base) if base == use_pos[2] else drop_normal(base)
-    servos[7].write(180)
     time.sleep(0.2)
     grab_ad(base) if base == use_pos[2] else grab_normal(base)
 
  
 def st3(base = 125):
-    servos[6].write(130)
+    servos[6].write(120) 
     drop_ad(base) if base == use_pos[2] else drop_normal(base)
-    servos[7].write(180)
+    servos[5].write(40)
     time.sleep(0.2)
-    grab_ad(base) if base == use_pos[2] else grab_normal(base)
+    pos(pos = [135,90,90,90,off_gripper])
+    # servos[7].write(180)
+    # grab_ad(base) if base == use_pos[2] else grab_normal(base)
+    time.sleep(0.5)
+    servos[8].write(off_gripper)
+    servos[7].write(180)
+    pos(pos = [base,90,180,180,0], ) # up
+    pos(pos = [base,90,180,163,0]) #bit up
+    input()
+    grab_ad(base)if base == use_pos[2] else grab_normal(base, n = 0)
 
 def drop(base = 30):
     time.sleep(0.3)
     servos[5].write(90)
-    pos(pos = [base,90,162,180,power_gripper]) # down
+    servos[7].write(90)
+    pos(pos = [base,90,162,90,power_gripper]) # down
+    servos[7].write(180)
     pos(pos = [base,10,162,180,power_gripper], step_delay=0.01)
-    time.sleep(1)
-    servos[8].write(off_gripper)
 
 def grab(base = 40):
-    servos[8].write(off_gripper)
     time.sleep(0.3)
     servos[7].write(155)
     servos[6].write(136)
-    servos[4].write(145, step_delay = 0.01)
-    servos[6].write(110)
+    servos[4].write(135, step_delay = 0.01)
     servos[8].write(10)
+    servos[7].write(145)
+    servos[6].write(110)
     servos[4].write(135)
 
     
@@ -117,16 +123,13 @@ servos[8].write(0)
  
 a = 82
 b = 184
-c = 239 
+c = 239
 base_= 33
-use_pos = [a,b,c][::-1]
+use_pos = [b,c,a]
 st1(base=use_pos[0])
 st2(base=use_pos[1])
 st3(base=use_pos[2])
 drop(base = base_)
 grab()
- 
- 
-# set90()
  
  
